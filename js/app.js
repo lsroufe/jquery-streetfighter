2c5db7b22ba5cde7a76352aedd3654ccff256079
Logan Sroufe <lsroufe@gmail.com>
2016-05-17 09:30:24 -0700

    updated app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-	$('.ryu').mouseenter(function() {
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
-	})
-	.mouseleave(function() {
-		$('.ryu-ready').hide();
-		$('.ryu-still').show();
-	});
+	$('.ryu')
+		.mouseenter(function() {
+			$('.ryu-still').hide();
+			$('.ryu-ready').show();
+		})
+		.mouseleave(function() {
+			$('.ryu-ready').hide();
+			$('.ryu-still').show();
+		});
 });
