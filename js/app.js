4d19f51a9a65462f77e5ebe7584ccdbccd255b24
Logan Sroufe <lsroufe@gmail.com>
2016-05-19 21:02:56 -0700

    updated html, css, and js

--- js/app.js
+++ js/app.js
@@ -27,6 +27,46 @@ $(document).ready(function() {
 			$('.ryu-throwing').hide();
 			$('.ryu-ready').show();
 		});
+		// .keydown(function (e) {
+		// 	if (e.which === 88) {
+		// 		$('.ryu-cool').show();
+		// 		$('.ryu-still').hide();
+		// 	}
+		// })
+		// .keyup(function (e) {
+		// 	if (e.which === 88) {
+		// 		$('.ryu-cool').hide();
+		// 		$('.ryu-still').show();
+		// 	}
+		// })
+		$('.hulk-ryu')
+		.mouseenter(function() {
+			$('.hulk-ryu-still').hide();
+			$('.hulk-ryu-ready').show();
+		})
+		.mouseleave(function() {
+			$('.hulk-ryu-ready').hide();
+			$('.hulk-ryu-still').show();
+		})
+		.mousedown(function() {
+			playHadouken();
+			$('.hulk-ryu-ready').hide();
+			$('.hulk-ryu-throwing').show();
+			$('.hulk-hadouken')
+				.finish()
+				.show()
+				.animate(
+					{'left': '1020px'},
+					500,
+					function() {
+						$(this).hide();
+						$(this).css('left', '520px');
+					});
+		})
+		.mouseup(function() {
+			$('.hulk-ryu-throwing').hide();
+			$('.hulk-ryu-ready').show();
+		});
 });
 
 function playHadouken () {
